6709240c632ad3b0652723c45b47238be0bdd41a
Tommy-Geenexus <tomgapplicationsdevelopment@gmail.com>
2025-09-01 16:38:35 +0200

    Release v2.2.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -26,8 +26,8 @@ android {
         applicationId = "io.github.tommygeenexus"
         minSdk = 35
         targetSdk = 36
-        versionCode = 21
-        versionName = "2.1.0"
+        versionCode = 22
+        versionName = "2.2.0"
     }
 
     signingConfigs {
